02c4da0b1c690af629981a399c8461d26e099d37
MariaEspFon <mariaespfon02@gmail.com>
2025-03-27 13:47:06 +0100

    variaciones en filtrado y en características espectrales
    filtrado paso bajo a 0.5Hz
    features_f_domain: power, bandwidth 99%, límite máximo del bw

--- MATLAB/features_f_domain.m
+++ MATLAB/features_f_domain.m
@@ -1,8 +1,18 @@
 function FreqFeatures = features_f_domain(s_entrada, fs)
     N = length(s_entrada);
     s_espectro = fft(s_entrada);
+    %{
+    Representación del espectro
+    S = abs(s_espectro/N);
+    S = S(1:N/2+1);
+    %como el espectro se ha reducido a un solo lado, se deben duplicar los
+    %coeficientes entre la frecuencia DC y la frecuencia de Nyquist
+    S(2:end-1) = 2*S(2:end-1);
+    f = fs*(0:(N/2))/N;
+    plot(f,S);
+    %}
     power = abs(s_espectro).^2/N; %densidad espectral de potencia
     pw = sum(power, 1); %potencia de cada ventana
     [bw, flo, fhi, pow99] = obw(s_entrada,fs);
-    FreqFeatures = [pw; flo; fhi];
+    FreqFeatures = [pw; bw; fhi];
 end
