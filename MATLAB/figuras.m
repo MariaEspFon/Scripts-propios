548259d9d2bc92eca4b245e0e9f41f31d41c9353
MariaEspFon <mariaespfon02@gmail.com>
2025-06-02 21:58:28 +0200

    corrección EMA y datasets fc=1Hz

--- MATLAB/figuras.m
+++ MATLAB/figuras.m
@@ -1,9 +1,24 @@
 registro = 18;
+signal = raw_signals{registro,2};
+time = linspace(0, (length(signal)-1), length(signal))/4;
 figure
-plot(phtn_signals{registro,3}) % tónica
+plot(time,signal);
 hold on
-plot(phtn_signals{registro,2}) % fásica
-plot(filtered_signals{registro,2}) % filtrada original
+registro = registro +1;
+signal = raw_signals{registro,2};
+time = linspace(0, (length(signal)-1), length(signal))/4;
+plot(time, signal);
+ylim([0 1.6])
 ylabel('Amplitud (\mu S)', 'FontWeight', 'bold')
 xlabel('Tiempo (s)', 'FontWeight', 'bold')
-legend('Componente tónica', 'Componente fasica', 'Señal EDA filtrada')
+ 
+
+
+% figure
+% plot(phtn_signals{registro,3}) % tónica
+% hold on
+% plot(phtn_signals{registro,2}) % fásica
+% plot(filtered_signals{registro,2}) % filtrada original
+% ylabel('Amplitud (\mu S)', 'FontWeight', 'bold')
+% xlabel('Tiempo (s)', 'FontWeight', 'bold')
+ %legend('EDA filtrada (Butterworth fc = 0.5Hz)', 'EDA sin filtro')
